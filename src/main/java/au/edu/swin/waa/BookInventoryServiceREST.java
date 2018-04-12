79a1e1bafd8aaac6096f0fc110213c0263666adc
Sandeep Singh Bansal <thisbansal@gmail.com>
2018-04-12 19:51:06 +1000

    Commented as much as possible. Everything works up to this point. 🤟

--- src/main/java/au/edu/swin/waa/BookInventoryServiceREST.java
+++ src/main/java/au/edu/swin/waa/BookInventoryServiceREST.java
@@ -1,24 +1,72 @@
 package au.edu.swin.waa;
 
 import java.sql.Connection;
-import java.sql.SQLException;
 
 public class BookInventoryServiceREST {
-	static int bookId ;
+	static int bookId;
 
+	/**
+	 * for testing the web service locally
+	 * this method will be deleted once pushed to ws02
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		BookInventoryServiceREST.borrowABookRequest(1234123, 101, 9867);
+		BookInventoryServiceREST.returnABook(1212);
 	}
+
 	// All the of the functions goes here
 
 	/**
-	 * Makes a book available for the student if found in the system.
-	 * If not, then advises the student to try requesting through google books
-	 * Also tells if student is present on the system. or If a valid student has entered a wrong secret pin
-	 * @param iSBNumberInteger int type for ISBN Number of requested book
-	 * @param studentIDInteger int type studentid
-	 * @param studentPin int type student's secret pin key
-	 * @return Returns a string stating whether a book has been borrowed successfully or if there's anything wrong with given input parameters
+	 * If a book is present in the system log and currently under borrowed state
+	 * if will set it to available
+	 * Also tells whether the given book belongs to the library or not
+	 * Also removes the entry from borrow table of a particular student for borrowed book
+	 * @param iSBNumberInteger ISBN Number of the the book of type Integer
+	 * @return Returns different strings referring whether book has been returned or not.
+	 */
+	public static String returnABook(Integer iSBNumberInteger) {
+		String resultString = "";
+		Connection conn = CRUDSupport.connectToDatabase();
+		String isValidBookString = CRUDSupport.isBookValid(conn,
+				iSBNumberInteger);
+		switch (isValidBookString) {
+		case "Borrowed": {
+			resultString = CRUDSupport.returnBook(conn, iSBNumberInteger);
+			CRUDSupport.closeConnection(conn);
+			switch (resultString) {
+			case "Successfuly Returned.":
+				return resultString;
+			default:
+				resultString = "Some error occured. Item couldn't be returned";
+				return resultString;
+			}
+		}
+		case "Book found": {
+			resultString = "Book is already present in the system.";
+			return resultString;
+		}
+		default:
+			resultString = "This Book doesn't belongs to this library.";
+		}
+		CRUDSupport.closeConnection(conn);
+		return resultString;
+	}
+	
+	/**
+	 * Makes a book available for the student if found in the system. If not,
+	 * then advises the student to try requesting through google books Also
+	 * tells if student is present on the system. or If a valid student has
+	 * entered a wrong secret pin
+	 * 
+	 * @param iSBNumberInteger
+	 *            Integer type for ISBN Number of requested book
+	 * @param studentIDInteger
+	 *            Integer type for StudentID
+	 * @param studentPin
+	 *            Integer type student's secret pin key
+	 * @return Returns a string stating whether a book has been borrowed
+	 *         successfully or if there's anything wrong with given input
+	 *         parameters
 	 */
 	public static String borrowABookRequest(Integer iSBNumberInteger,
 			Integer studentIDInteger, Integer studentPin) {
@@ -26,42 +74,40 @@ public class BookInventoryServiceREST {
 		Connection conn = CRUDSupport.connectToDatabase();
 		resultString = CRUDSupport.isStudentValid(conn, studentIDInteger,
 				studentPin);
-		System.out.println("isValidStudent status: " + resultString);
-		//switches over the results retrieved by CRUDSupport class for definitive result string for student validation
+		// switches over the results retrieved by CRUDSupport class for
+		// definitive result string for student validation
 		switch (resultString) {
 		case "Invalid Student ID":
 			return resultString;
 		case "Pin code doesn't matches":
 			return resultString;
 		}
-		resultString = CRUDSupport.isBookValid(conn, iSBNumberInteger,studentIDInteger);
-		System.out.println("isBookAvailStatus: " + resultString);
-		//switches over the results retrieved by CRUDSupport class for definitive result string for book borrow request
+		resultString = CRUDSupport.isBookValid(conn, iSBNumberInteger);
+		// switches over the results retrieved by CRUDSupport class for
+		// definitive result string for borrow book request
 		switch (resultString) {
-			case "Book found":
-			{
-				System.out.println("Book has been successfully borrowed by the student ("
-								+ studentIDInteger.toString() + ")");
-				try {
-					CRUDSupport.makeBorrowedPermanent(conn, iSBNumberInteger,
-							bookId, studentIDInteger);
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-				resultString += " " + studentIDInteger.toString();
-				return resultString;
-			
-			}
-			case "Invalid Book ID":
-				resultString = "No Book found with given ISBN number. Try requesting from Google Book.";
-				break;
-			case "borrowed":
-				resultString = "Book is currently not availbale to be borrowed.";
-				break;
+		case "Book found": {
+			System.out
+					.println("Book has been successfully borrowed by the student ("
+							+ studentIDInteger.toString() + ")");
+			CRUDSupport.makeBorrowedPermanent(conn, iSBNumberInteger, bookId,
+					studentIDInteger);
+			CRUDSupport.closeConnection(conn);
+			resultString += " " + studentIDInteger.toString();
+			return resultString;
+
 		}
+		case "Invalid Book ID":
+			resultString = "No Book found with given ISBN number. Try requesting from Google Book.";
+			break;
+		case "borrowed":
+			resultString = "Book is currently not availbale to be borrowed.";
+			break;
+		}
+		CRUDSupport.closeConnection(conn);
 		return resultString;
 	}
-
+	
 	/**
 	 * Request a copy of the requested book from Google Books If a books is
 	 * available, BookInvertory will also add it to their database for the
@@ -79,4 +125,5 @@ public class BookInventoryServiceREST {
 		return isBookAvailableByTheUniversity;
 	}
 
+	
 }
--- src/main/java/au/edu/swin/waa/CRUDSupport.java
+++ src/main/java/au/edu/swin/waa/CRUDSupport.java
@@ -7,50 +7,56 @@ import java.sql.Statement;
 import java.util.Properties;
 import java.sql.Connection;
 
-import javafx.collections.ListChangeListener.Change;
 
 public class CRUDSupport {
-	
+
 	/**
 	 * Establishes a connection between the application and the database
+	 * 
 	 * @return A copy of the Connection object referring to the database
 	 */
-	public static Connection connectToDatabase(){
+	public static Connection connectToDatabase() {
 		Connection connectionObject = null;
 		Properties connectionProperties = new Properties();
 		connectionProperties.put("user", "waaWebService1");
-		connectionProperties.put("password", "Assignm3nt1");			
-		String databseUrl= "jdbc:mysql://swinmysql-instance1.csfetredihe2.us-east-1.rds.amazonaws.com/";
+		connectionProperties.put("password", "Assignm3nt1");
+		String databseUrl = "jdbc:mysql://swinmysql-instance1.csfetredihe2.us-east-1.rds.amazonaws.com/";
 		try {
-			connectionObject = DriverManager.getConnection(databseUrl, connectionProperties);
+			connectionObject = DriverManager.getConnection(databseUrl,
+					connectionProperties);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return connectionObject;
 	}
-	
+
 	/**
-	 * Check whether a student is valid or not
-	 * Sends different Strings based on outcome of the query
-	 * @param conn connection to the database.
-	 * @param studentID student ID of the student.
-	 * @param studentPinCode pin code of the student.
+	 * Check whether a student is valid or not Sends different Strings based on
+	 * outcome of the query
+	 * 
+	 * @param conn
+	 *            connection to the database.
+	 * @param studentID
+	 *            student ID of the student.
+	 * @param studentPinCode
+	 *            pin code of the student.
 	 * @return Returns a string for invalid student id or pin code
 	 */
-	public static String isStudentValid(Connection conn, Integer studentID, Integer studentPinCode){
+	public static String isStudentValid(Connection conn, Integer studentID,
+			Integer studentPinCode) {
 		String result = "Invalid Student ID";
 		Statement stmt = null;
-		String queryString = "SELECT `idStudent`,`pinCodeStudent` FROM `waaBookInventoryDB`.`Student` WHERE `idStudent` = "+studentID.toString();
+		String queryString = "SELECT `idStudent`,`pinCodeStudent` FROM `waaBookInventoryDB`.`Student` WHERE `idStudent` = "
+				+ studentID.toString();
 		try {
 			stmt = conn.createStatement();
 			ResultSet resultSet = stmt.executeQuery(queryString);
-			while (resultSet.next()){
+			while (resultSet.next()) {
 				int id = resultSet.getInt(1);
 				int pinCode = resultSet.getInt(2);
-				if (id == studentID && pinCode == studentPinCode){
+				if (id == studentID && pinCode == studentPinCode) {
 					result = "Succesfully Logged In";
-				}
-				else if (id == studentID) {
+				} else if (id == studentID) {
 					result = "Pin code doesn't matches";
 				}
 			}
@@ -68,23 +74,38 @@ public class CRUDSupport {
 		return result;
 	}
 
-	public static String isBookValid(Connection conn, Integer iSBNumberInteger, Integer studentIdString) {
+	/**
+	 * Checks whether a book is valid or not
+	 * 
+	 * @param conn
+	 *            Connection to the database
+	 * @param iSBNumberInteger
+	 *            ISBN Number of the requested book
+	 * @param studentIdString
+	 *            StudentID of the the student
+	 * @return Returns different sets of strings whether it is a valid, invalid
+	 *         or borrowed
+	 */
+	public static String isBookValid(Connection conn, Integer iSBNumberInteger) {
 		String result = "Invalid Book ID";
 		Statement stmt = null;
-		String queryString = "SELECT `ISBN`,`AVAILABILITY`,`idBook` FROM `waaBookInventoryDB`.`Book` WHERE `ISBN` = "+iSBNumberInteger.toString();
+		String queryString = "SELECT `ISBN`,`AVAILABILITY`,`idBook` FROM `waaBookInventoryDB`.`Book` WHERE `ISBN` = "
+				+ iSBNumberInteger.toString();
 		try {
 			stmt = conn.createStatement();
 			ResultSet resultSet = stmt.executeQuery(queryString);
-			while (resultSet.next()){
+			while (resultSet.next()) {
 				int isbn = resultSet.getInt(1);
 				String availabilityString = resultSet.getString(2);
 				int idBook = resultSet.getInt(3);
-				if (isbn == iSBNumberInteger && availabilityString.equalsIgnoreCase("available")){
+				if (isbn == iSBNumberInteger
+						&& availabilityString.equalsIgnoreCase("available")) {
 					result = "Book found";
 					BookInventoryServiceREST.bookId = idBook;
-				}
-				else if (isbn == iSBNumberInteger && availabilityString.equalsIgnoreCase("Borrowed")) {
-					result = "borrowed";
+				} else if (isbn == iSBNumberInteger
+						&& availabilityString.equalsIgnoreCase("Borrowed")) {
+					result = "Borrowed";
+					BookInventoryServiceREST.bookId = idBook;
 					break;
 				}
 			}
@@ -102,15 +123,87 @@ public class CRUDSupport {
 		return result;
 	}
 
-	public static void makeBorrowedPermanent(Connection conn, Integer iSBNumberInteger, int idBook, int studentId) throws SQLException {
-		Statement stmt = conn.createStatement();;
-		String stringBorrowQuery = "UPDATE `waaBookInventoryDB`.`Book` SET `Availability`='Borrowed' WHERE `ISBN`='"+iSBNumberInteger+"'";
-		String stringJoinQuery   = "INSERT INTO `waaBookInventoryDB`.`BooksBorrowed`(`idBook`,`idStudent`)VALUES("+idBook+","+studentId+")";
-		stmt.addBatch(stringBorrowQuery);
-		stmt.addBatch(stringJoinQuery);
-		int[] recordsAffected = stmt.executeBatch();
-		for (int i: recordsAffected){
-			System.out.println("records affected: "+i);
+	/**
+	 * Returns the said book
+	 * @param conn Connection to the database
+	 * @param iSBNNumberInteger ISBN Number of the provided book
+	 * @return String with information about the returning book
+	 */
+	public static String returnBook(Connection conn, Integer iSBNNumberInteger){
+		String resultString = "Some error occured. Item couldn't be returned.";
+		String queryStringForChangingFieldValue = "UPDATE `waaBookInventoryDB`.`Book` SET `Availability`='Available' WHERE `ISBN`='"
+				+ iSBNNumberInteger + "'";
+		String deleteColumnFromBorrowTableString = "DELETE FROM `waaBookInventoryDB`.`BooksBorrowed`WHERE idBook = "+BookInventoryServiceREST.bookId;
+		Statement stmtStatement = null;
+		try {
+			stmtStatement = conn.createStatement();
+			stmtStatement.addBatch(queryStringForChangingFieldValue);
+			stmtStatement.addBatch(deleteColumnFromBorrowTableString);
+			stmtStatement.executeBatch();
+			resultString = "Successfuly Returned.";
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				if (stmtStatement!=null)
+					stmtStatement.close();
+			} catch (SQLException e2) {
+				e2.printStackTrace();
+			}
+		}
+		return resultString;
+	}
+	
+	/**
+	 * Updates the required tables to reflect the changes on the back head. Uses
+	 * SQLBatch execution to update multiple tables
+	 * 
+	 * @param conn
+	 *            Connection to the database
+	 * @param iSBNumberInteger
+	 *            ISBN Number of the requested Book
+	 * @param idBook
+	 *            ID of the book on the table
+	 * @param studentId
+	 *            StudentID given by the student.
+	 */
+	public static void makeBorrowedPermanent(Connection conn,
+			Integer iSBNumberInteger, int idBook, int studentId) {
+		Statement stmt = null;
+		try {
+			stmt = conn.createStatement();
+			String stringBorrowQuery = "UPDATE `waaBookInventoryDB`.`Book` SET `Availability`='Borrowed' WHERE `ISBN`='"
+					+ iSBNumberInteger + "'";
+			String stringJoinQuery = "INSERT INTO `waaBookInventoryDB`.`BooksBorrowed`(`idBook`,`idStudent`)VALUES("
+					+ idBook + "," + studentId + ")";
+			stmt.addBatch(stringBorrowQuery);
+			stmt.addBatch(stringJoinQuery);
+			int[] recordsAffected = stmt.executeBatch();
+			for (int i : recordsAffected) {
+				System.out.println("records affected: " + i);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Closes the connection to the database securely
+	 * @param conn object which needs to be closed
+	 */
+	public static void closeConnection(Connection conn) {
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
 }
